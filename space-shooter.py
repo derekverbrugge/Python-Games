ea4718bede5fad058e7bc8279a4f5cb2d8daf95e
Derek Verbrugge <52356885+derekverbrugge@users.noreply.github.com>
2022-12-04 21:29:04 -0500

    Update space-shooter.py
    In this updated version of the program, the player's spaceship can shoot bullets using the space bar. The bullets move upwards towards the enemy aliens, and when a bullet collides with an alien, both the bullet and the alien are removed from the game.
    
    To use this program, you will need to have the pygame library installed, as well as image files named player.png, bullet.png, and enemy.png in the same directory as the program. You can customize the game by changing the values of the various game variables, such as the screen size, player and bullet speeds, and image files used.

--- space-shooter.py
+++ space-shooter.py
@@ -18,6 +18,11 @@ player_image = pygame.image.load("player.png")
 player_pos = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50)
 player_speed = 5
 
+# Set up the player's bullets
+bullet_image = pygame.image.load("bullet.png")
+bullet_speed = 10
+bullets = []
+
 # Set up the enemy aliens
 enemy_image = pygame.image.load("enemy.png")
 enemy_pos = (random.randint(0, SCREEN_WIDTH - 50), 0)
@@ -36,28 +41,46 @@ while running:
             if event.key == pygame.K_RIGHT:
                 player_pos = (player_pos[0] + player_speed, player_pos[1])
             if event.key == pygame.K_SPACE:
-                pass  # TODO: Implement player shooting
+                # Create a new bullet and add it to the list of bullets
+                bullet = {
+                    "pos": (player_pos[0] + 25, player_pos[1]),  # Center the bullet horizontally
+                    "vel": bullet_speed,
+                }
+                bullets.append(bullet)
 
     # Update the enemy's position
     enemy_pos = (enemy_pos[0], enemy_pos[1] + enemy_speed)
     if enemy_pos[1] > SCREEN_HEIGHT:
         enemy_pos = (random.randint(0, SCREEN_WIDTH - 50), 0)
 
-    # TODO: Implement collision detection
+    # Update the position of each bullet
+    for bullet in bullets:
+        bullet["pos"] = (bullet["pos"][0], bullet["pos"][1] - bullet["vel"])
+
+    # Check for collisions between bullets and the enemy
+    for bullet in bullets:
+        if (
+            bullet["pos"][0] >= enemy_pos[0]
+            and bullet["pos"][0] <= enemy_pos[0] + 50
+            and bullet["pos"][1] >= enemy_pos[1]
+            and bullet["pos"][1] <= enemy_pos[1] + 50
+        ):
+            # Remove the bullet and the enemy when they collide
+            bullets.remove(bullet)
+            enemy_pos = (random.randint(0, SCREEN_WIDTH - 50), 0)
 
     # Draw the game objects on the screen
-    screen.fill((0, 0, 0))  # Clear the screen
+        screen.fill((0, 0, 0))  # Clear the screen
     screen.blit(player_image, player_pos)  # Draw the player's spaceship
     screen.blit(enemy_image, enemy_pos)  # Draw the enemy aliens
+
+    # Draw each bullet
+    for bullet in bullets:
+        screen.blit(bullet_image, bullet["pos"])
+
     pygame.display.flip()  # Update the screen
 
     # Limit the frame rate
     clock.tick(60)
     
-# This program sets up a basic space shooter game, with a player's spaceship that can be controlled using the left and right arrow keys, 
-# and enemy aliens that move down the screen. The program does not yet include features such as shooting or collision detection, 
-# which you can add as needed.
 
-# To use this program, you will need to install the pygame library and have an image file named player.png and enemy.png 
-# in the same directory as the program. You can customize the game by changing the values of the various game variables, 
-# such as the screen size, player and enemy speeds, and image files used.
